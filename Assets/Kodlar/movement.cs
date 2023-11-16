a048de59adccd2f97abf0c400f9a968418e8b96f
AltanTuran <haltanturan@gmail.com>
2023-11-16 23:23:15 +0300

    sert

--- Assets/Kodlar/movement.cs
+++ Assets/Kodlar/movement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rigid;
+    Animator animator;
+    public float speed = 5;
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Hareket();
+        RotateTowardsMouse();
+        if(rigid.velocity != Vector2.zero)
+        {
+            animator.SetBool("movement", true);
+        }
+        else { animator.SetBool("movement", false); }
+    }
+
+    void Hareket()
+    {
+        rigid.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+    }
+    void RotateTowardsMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePosition - transform.position;
+        direction.Normalize();
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
     }
 }
